09c8e661831b6398cac7ab4f7634fdd68fa6c53e
Joe Malebe <Joe.Malebe@standardbank.co.za>
2024-02-16 17:01:36 +0200

    add navigation bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import '../App.scss';
+import '../NavBar.scss';
+function NavBar() {
+   return <>
+        <div className="NavBar">
+            <a href="#Projects">Projects</a>
+            <a href="#Experience">Experience</a>
+            <a href="#Skills">Skills</a>
+            <a href="#Home">Home</a>
+        </div>
+    </>
+}
+
+export default NavBar
