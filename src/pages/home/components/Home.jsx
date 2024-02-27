2534321356c21d3dd18c065b2570c7d588980149
Joe Malebe <Joe.Malebe@standardbank.co.za>
2024-02-27 20:44:46 +0200

    create new file structure for home

---
+++ src/pages/home/components/Home.jsx
@@ -0,0 +1,32 @@
+import homeImage from "../assets/homeImg.png";
+import SpinningTagCloud from "./SpinningTagCloud";
+import "./Home.scss";
+function Home() {
+  return (
+    <>
+      <div className="container-fluid Page">
+        <div className="row ">
+          <div className="col col-lg">
+            <div className="vstack gap-3">
+              <div className='Slogan'>
+                <h4 className="line2 anim-typewriter2 SloganBody"> Software Engineer </h4>
+                <p className="line1 anim-typewriter1 SloganBody">
+                Engineer. Innovate. Lead.
+                </p>
+              </div>
+              <div>
+                <SpinningTagCloud  />
+              </div>
+            </div>
+          </div>
+
+          <div className="col col-lg">
+            <img src={homeImage} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,8 @@
+import Home from "./components/Home";
+export function HomePage() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
