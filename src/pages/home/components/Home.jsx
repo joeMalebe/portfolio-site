630071ac61badbf5533b21ee58b11618f99c132d
Joe Malebe <Joe.Malebe@standardbank.co.za>
2024-03-04 21:58:45 +0200

    display social media icons

--- src/pages/home/components/Home.jsx
+++ src/pages/home/components/Home.jsx
@@ -1,12 +1,39 @@
 import homeImage from "../assets/homeImg.png";
 import SpinningTagCloud from "./SpinningTagCloud";
-import { SocialIcon } from 'react-social-icons';
+import { SocialIcon } from "react-social-icons";
 import "./Home.scss";
-const Github = <SocialIcon url="https://github.com/joeMalebe" bgColor="currentColor" />
-const LinkedIn = <SocialIcon url="https://www.linkedin.com/in/joe-malebe-08677478/" bgColor="currentColor"  className="SocialIcon"/>
-const StackOverflow = <SocialIcon url="https://stackoverflow.com/users/4377454/joe-malebe/" bgColor="currentColor"/>
-const AppStore = <SocialIcon network="google_play" url="https://play.google.gmail/apps/details?id=za.co.app.budgetbee&hl=en&gl=US" bgColor="currentColor"/>
-const Email = <SocialIcon network="email" url="mailto:jtekniq@gmail.com" bgColor="currentColor"/>
+
+const style = { width: 25, height: 25, margin: 6 };
+
+const Github = (
+  <SocialIcon
+    url="https://github.com/joeMalebe"
+    bgColor="black"
+    style={style}
+  />
+);
+const LinkedIn = (
+  <SocialIcon
+    url="https://www.linkedin.com/in/joe-malebe-08677478/"
+    style={style}
+  />
+);
+const StackOverflow = (
+  <SocialIcon
+    url="https://stackoverflow.com/users/4377454/joe-malebe/"
+    style={style}
+  />
+);
+const AppStore = (
+  <SocialIcon
+    network="google_play"
+    url="https://play.google.gmail/apps/details?id=za.co.app.budgetbee&hl=en&gl=US"
+    style={style}
+  />
+);
+const Email = (
+  <SocialIcon network="email" url="mailto:jtekniq@gmail.com" style={style} />
+);
 
 function Home() {
   return (
@@ -15,22 +42,24 @@ function Home() {
         <div className="row ">
           <div className="col col-lg">
             <div className="vstack gap-3">
-              <div className='Slogan'>
-                <h4 className="line2 anim-typewriter2 SloganBody"> Software Engineer </h4>
+              <div className="Slogan">
+                <h4 className="line2 anim-typewriter2 SloganBody">
+                  {" "}
+                  Software Engineer{" "}
+                </h4>
                 <p className="line1 anim-typewriter1 SloganBody">
-                Engineer. Innovate. Lead.
+                  Engineer. Innovate. Lead.
                 </p>
               </div>
               <div>
-                <SpinningTagCloud  />
-                <span>
-                {Github}
-                {LinkedIn}
-                {StackOverflow}
-                {AppStore}
-                {Email}
+                <SpinningTagCloud />
+                <span className="SocialIcons">
+                  {Github}
+                  {LinkedIn}
+                  {StackOverflow}
+                  {AppStore}
+                  {Email}
                 </span>
-                
               </div>
             </div>
           </div>
