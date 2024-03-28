c8bd245f80fc57e720f3e0ed76a204db585b6d86
Joe Malebe <Joe.Malebe@standardbank.co.za>
2024-03-28 16:18:02 +0200

    add clearscore experience item

--- src/pages/experience/components/ExperiencePage.jsx
+++ src/pages/experience/components/ExperiencePage.jsx
@@ -38,7 +38,8 @@ const experienceList = (experienceList) => {
 
 const ExperienceItem = (experience) => {
   return <>
-          <div className="col-lg-2">
+          <div className="row" style={{paddingBottom: "3em"}}>
+          <div className="col-lg-2" >
             {}
             <h3>{experience.companyName}</h3>
             <h4>{experience.timeAtCompany}</h4>
@@ -58,13 +59,14 @@ const ExperienceItem = (experience) => {
           <div className="col-lg-10">
             {ExperienceRolesAndResponsibility(experience.experienceDescription)}
           </div>
+          </div>
         </>
 }
 
 function Experience() {
   return (
     <>
-      <div className="container-fluid">
+      <div className="container-fluid" style={{paddingTop : "4em"}}>
         <div className="row">
          {experienceList(getExperiences)}
         </div>
--- src/pages/experience/data/WorkExperience.js
+++ src/pages/experience/data/WorkExperience.js
@@ -43,6 +43,7 @@ const getExperiences = [
       { value: "Retrofit" },
       { value: "RxJava" },
       { value: "SpringBoot" },
+      { value: "Kotlin"},
       { value: "Java" },
       { value: "Dynatrace" },
       { value: "Google analytics" },
@@ -51,6 +52,54 @@ const getExperiences = [
       { value: "Google maps" },
     ]
   ),
+  new WorkExperience(
+    "ClearScore",
+    [
+      new DescriptionPoint(
+        "Team of the Year Award:",
+        "Honored as part of the award-winning team, recognized for our outstanding contributions to ClearScore's success. Our consistent delivery of major projects to diverse markets underscored our team's commitment to excellence and innovation."
+      ),
+      new DescriptionPoint(
+        "Backend Service Development:",
+        "Collaborated on the design, development, and maintenance of Node feature services tailored to cater to the needs of mobile clients across iOS and Android platforms. Utilized JavaScript to craft robust and scalable backend solutions, ensuring seamless integration with mobile applications."
+      ),
+
+      new DescriptionPoint(
+        "Cross-Platform Collaboration:",
+        "Engaged in cross-functional collaboration, working closely with colleagues from diverse backgrounds to deliver feature-rich experiences to users. Played a pivotal role in bridging the gap between mobile and backend development teams, facilitating efficient communication and project execution."
+      ),
+
+      new DescriptionPoint(
+        "Technology Stack Mastery:",
+        "Expanded my skill set by gaining hands-on experience with Scala, contributing to the development of specialized backend services for local clients. This diversification of expertise enabled me to deliver tailored solutions to meet the unique requirements of different markets."
+      ),
+
+      new DescriptionPoint(
+        "Feature Delivery:",
+        "Led the delivery of key features, including in-app notifications, home page refresh functionality, and Betterscore enhancements, targeting ClearScore's South African and Australian markets. These initiatives not only enriched the user experience but also contributed to the company's market expansion efforts."
+      ),
+      new DescriptionPoint(
+        "Performance Monitoring:",
+        "Implemented robust performance monitoring solutions, leveraging tools such as Amplitude and SignalFx to meticulously monitor feature performance in production environments. Proactively identified and addressed performance bottlenecks, ensuring optimal user experience and system reliability."
+      ),
+      new DescriptionPoint(
+        "Technology Migration:",
+        "Embraced Kotlin as a strategic initiative to modernize the Discovery Member App. Actively participated in the migration process, mastering Kotlin's nuances and serving as a key reviewer during the transition phase. Contributed invaluable insights and expertise to streamline the migration process, resulting in enhanced codebase maintainability and developer productivity."
+      ),
+    ],
+    "1 year",
+    [
+      { value: "Coroutines" },
+      { value: "Scala" },
+      { value: "Jetpack Compose" },
+      { value: "Amplitude" },
+      { value: "SignalFx" },
+      { value: "Mvvm" },
+      { value: "Javascript" },
+      { value: "Node" },
+      { value: "Kotlin" },
+    ]
+  )
 ];
 
 export default getExperiences;
