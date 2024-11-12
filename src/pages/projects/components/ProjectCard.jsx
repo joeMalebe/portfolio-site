927e831c0d69b7286c55ebdb707dd273d546a988
Joe Malebe <Joe.Malebe@standardbank.co.za>
2024-11-12 14:36:33 +0200

    update projects styling

--- src/pages/projects/components/ProjectCard.jsx
+++ src/pages/projects/components/ProjectCard.jsx
@@ -1,9 +1,6 @@
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
 import "../components/ProjectCards.scss";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 
 const styles = {
 cardImage: {
